6164bef6140444f15d1bc2354287a3912b9a13b5
ivanp___ <rafa.pejovic@gmail.com>
2023-08-06 23:24:47 +0200

    Add project files.

---
+++ WGO_MNE.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WGO_MNE.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
