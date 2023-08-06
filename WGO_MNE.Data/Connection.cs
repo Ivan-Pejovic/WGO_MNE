950fe3ea6665d0d375f957bb3a495ac96d2b8ae6
ivanp___ <rafa.pejovic@gmail.com>
2023-08-07 00:11:02 +0200

    Installed MySql.Data and created a connection class

---
+++ WGO_MNE.Data/Connection.cs
@@ -0,0 +1,25 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace WGO_MNE.Data
+{
+    public class Connection
+    {
+        public MySqlConnection sqlConn;
+        public const string datasourceURL = "Server=IVANP\\SQLEXPRESS;Database=WGO_MNE;Integrated Security=True;TrustServerCertificate=True";
+
+        public Connection()
+        {
+            sqlConn = new MySqlConnection(datasourceURL);
+        }
+
+        public void Open()
+        {
+            sqlConn.Open();
+        }
+
+        public void Close()
+        {
+            sqlConn.Close();
+        }
+    }
+}
