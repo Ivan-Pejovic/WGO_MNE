f97c3a04b54c338073cda0c1973820f3ea94a901
ivanp___ <rafa.pejovic@gmail.com>
2023-08-08 17:55:05 +0200

    Created Post Model

---
+++ WGO_MNE.Logic/Models/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WGO_MNE.Logic.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public User Author { get; set; }
+        public string Text { get; set; }
+        public string Location { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
