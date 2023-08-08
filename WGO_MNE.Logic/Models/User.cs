7d68edcbb81e445f7972960a747db13f3b337b2f
ivanp___ <rafa.pejovic@gmail.com>
2023-08-08 17:53:14 +0200

    Created User Model and added Project references from Web to Logic and from Logic to Data

---
+++ WGO_MNE.Logic/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WGO_MNE.Logic.Models
+{
+    public class User
+    {
+        public int Id { private set; get; }
+        public string FirstName { private set; get; }
+        public string LastName { private set; get; }
+        public string Username { private set; get; }
+        public string Biography { private set; get; }
+        public string Email { private set; get; }
+        public string Password { private set; get; }
+        public Country Country { private set; get; }
+        public string ProfilePicture { private set; get; }
+
+    }
+}
