ca3df7cc3c6eb1fbe94c25ae21959482bf012dcf
ivanp___ <rafa.pejovic@gmail.com>
2023-08-12 02:33:59 +0200

    Created UserDAL interface

---
+++ WGO_MNE.Logic/Interfaces/IUserDAL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WGO_MNE.Logic.Models;
+
+namespace WGO_MNE.Logic.Interfaces
+{
+    public interface IUserDAL
+    {
+        public bool Insert(User newUser);
+        public List<User> GetAll();
+        public User GetById(int id);
+        public bool Update(User updatedUser);
+        public bool Delete(User userToDelete);
+        public User Login(string username, string password);
+    }
+}
