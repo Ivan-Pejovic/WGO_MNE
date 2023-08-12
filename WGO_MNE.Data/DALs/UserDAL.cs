28a0513d7af1f8f3d1043468d9c8336e6e6bb72f
ivanp___ <rafa.pejovic@gmail.com>
2023-08-12 02:40:29 +0200

    Implemented Interface methods in UserDAL and changed project references

---
+++ WGO_MNE.Data/DALs/UserDAL.cs
@@ -0,0 +1,38 @@
+﻿using WGO_MNE.Logic.Interfaces;
+using WGO_MNE.Logic.Models;
+
+namespace WGO_MNE.Data.DALs
+{
+    public class UserDAL : IUserDAL
+    {
+        public bool Insert(User newUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(User updatedUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(User userToDelete)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Login(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
