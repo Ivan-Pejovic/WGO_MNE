40cb03213a5e0d5818ebce5eeff668db1d5eb56a
ivanp___ <rafa.pejovic@gmail.com>
2023-08-14 21:54:06 +0200

    Created CountryMapper

---
+++ WGO_MNE.Logic/Mappers/CountryMapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WGO_MNE.Logic.DTOs;
+using WGO_MNE.Logic.Models;
+
+namespace WGO_MNE.Logic.Mappers
+{
+    public static class CountryMapper
+    {
+        public static Country ToModel(CountryDTO dto)
+        {
+            return new Country(dto.Id, dto.Name, dto.Icon);
+        }
+
+        public static CountryDTO ToDTO(Country country)
+        {
+            return new CountryDTO(country.Id, country.Name, country.Icon);
+        }
+    }
+}
