cf39b2b1628a9c553914b0a45281ee68520a655d
ivanp___ <rafa.pejovic@gmail.com>
2023-08-14 21:40:54 +0200

    Created CountryDAL interface

---
+++ WGO_MNE.Logic/Interfaces/ICountryDAL.cs
@@ -0,0 +1,13 @@
+﻿using WGO_MNE.Logic.DTOs;
+
+namespace WGO_MNE.Logic.Interfaces
+{
+    public interface ICountryDAL
+    {
+        public bool Insert(CountryDTO newCountry);
+        public List<CountryDTO> GetAll();
+        public CountryDTO GetById(int id);
+        public bool Update(CountryDTO updatedCountry);
+        public bool Delete(CountryDTO countryToDelete);
+    }
+}
