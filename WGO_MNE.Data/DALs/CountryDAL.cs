3b3b636680803b4d02ead7f71beae625288b95bf
ivanp___ <rafa.pejovic@gmail.com>
2023-08-14 21:41:58 +0200

    Created CountryDAL

---
+++ WGO_MNE.Data/DALs/CountryDAL.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WGO_MNE.Logic.DTOs;
+using WGO_MNE.Logic.Interfaces;
+
+namespace WGO_MNE.Data.DALs
+{
+    public class CountryDAL : ICountryDAL
+    {
+        private Connection _connection;
+
+        public CountryDAL()
+        {
+            _connection = new Connection();
+        }
+
+        public bool Insert(CountryDTO newCountry)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CountryDTO> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CountryDTO GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(CountryDTO updatedCountry)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(CountryDTO countryToDelete)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
