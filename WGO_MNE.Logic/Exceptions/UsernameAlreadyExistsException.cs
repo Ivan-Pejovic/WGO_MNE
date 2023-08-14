96d31fbbd9417b56067d05e51e4db64eb1c3436e
ivanp___ <rafa.pejovic@gmail.com>
2023-08-14 21:16:35 +0200

    Created UsernameAlreadyExistsException

---
+++ WGO_MNE.Logic/Exceptions/UsernameAlreadyExistsException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WGO_MNE.Logic.Exceptions
+{
+    public class UsernameAlreadyExistsException : Exception
+    {
+        public UsernameAlreadyExistsException() { }
+        public UsernameAlreadyExistsException(string message) : base(message) { }
+        public UsernameAlreadyExistsException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
